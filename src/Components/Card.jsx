96057736051f20fb655b985b205dca38225000e2
masinde sylvester <masinde.sylvester@yahoo.com>
2024-11-12 00:29:32 +0300

    initial commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Card = ({ children ,bg='bg-neutral-300'}) => {
+const Card = ({ children ,bg='bg-slate-300'}) => {
   return (
     <div className={`${bg} p-6 rounded-md shadow-md`}>
         {children}
--- src/Components/HomeCards.jsx
+++ src/Components/HomeCards.jsx
@@ -13,19 +13,19 @@ const HomeCards = () => {
                   </p>
                   <a
                     href="/jobs.html"
-                    className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+                    className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-blue-900"
                   >
                     Browse Jobs
                   </a>
                 </Card>
-                <Card bg='bg-indigo-100'>
+                <Card>
                 <h2 className="text-2xl font-bold">For Employers</h2>
                   <p className="mt-2 mb-4">
                     List your job to find the perfect developer for the role
                   </p>
                   <a
                     href="/add-job.html"
-                    className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+                    className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-blue-900"
                   >
                     Add Job
                   </a>
