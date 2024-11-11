8d14ea3d326ad154814dab124de87c3f49a4b4e2
masinde sylvester <masinde.sylvester@yahoo.com>
2024-11-12 00:47:08 +0300

    initial commit

--- src/Components/JobListings.jsx
+++ src/Components/JobListings.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import jobs from '../jobs.json';
+import JobListing from './JobListing';
 
 const JobListings = () => {
   return (
@@ -10,33 +11,7 @@ const JobListings = () => {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {jobs.map((job) => (
-            <div key={job.id} className="bg-slate-300 rounded-xl shadow-md relative">
-              <div className="p-4">
-                <div className="mb-6">
-                  <div className="text-gray-600 my-2">{job.type}</div>
-                  <h3 className="text-xl font-bold">{job.title}</h3>
-                </div>
-
-                <div className="mb-5">{job.description}</div>
-
-                <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
-
-                <div className="border border-gray-100 mb-5"></div>
-
-                <div className="flex flex-col lg:flex-row justify-between mb-4">
-                  <div className="text-orange-700 mb-3">
-                    <i className="fa-solid fa-location-dot text-lg"></i>
-                    { job.location }
-                  </div>
-                  <a
-                    href="job.html"
-                    className="h-[36px] inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-blue-900"
-                  >
-                    Read More
-                  </a>
-                </div>
-              </div>
-            </div>
+            <JobListing key={job.id} job={job} />
           ))}
         </div>
       </div>
