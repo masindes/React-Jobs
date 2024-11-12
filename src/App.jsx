48513bc4cfb84c286bf80364ab7e17a3725f7c81
masinde sylvester <masinde.sylvester@yahoo.com>
2024-11-12 20:13:56 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import { Route, createBrowserRouter,createRoutesFromElements, RouterProvider,
 } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import HomePage from './Pages/HomePage';
+import MainLayout from './Layouts/MainLayout';
+import JobsPage from './Pages/JobsPage';
+
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<HomePage />} />),
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+  <Route index element={<HomePage />} />
+  <Route path='/jobs' element={<JobsPage />} />
+  </Route>),
 );
 
 const App = () => {
