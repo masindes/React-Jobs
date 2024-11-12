770d34b6079a43bf44b6eac2749e762b8da01bf9
masinde sylvester <masinde.sylvester@yahoo.com>
2024-11-12 12:20:10 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import HomeCards from './Components/HomeCards';
 import JobListings from './Components/JobListings';
+import ViewAllJobs from './Components/ViewAllJobs';
 
 
 import React from 'react'
@@ -15,13 +16,7 @@ const App = () => {
       <Hero />
       <HomeCards />
       <JobListings />
-          <section className="m-auto max-w-lg my-10 px-6">
-            <a
-              href="jobs.html"
-              className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-              >View All Jobs</a
-            >
-          </section>
+      <ViewAllJobs />
      </>
   );
 };
