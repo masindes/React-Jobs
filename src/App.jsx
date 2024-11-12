45118fdfb5255399db98f09b9e6a3abe33430235
masinde sylvester <masinde.sylvester@yahoo.com>
2024-11-12 21:22:25 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import HomePage from './Pages/HomePage';
 import MainLayout from './Layouts/MainLayout';
 import JobsPage from './Pages/JobsPage';
+import NotFoundPage from './Pages/NotFoundPage';
 
 
 const router = createBrowserRouter(
@@ -12,6 +13,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout />}>
   <Route index element={<HomePage />} />
   <Route path='/jobs' element={<JobsPage />} />
+  <Route path='*' element={<NotFoundPage />} />
   </Route>),
 );
 
